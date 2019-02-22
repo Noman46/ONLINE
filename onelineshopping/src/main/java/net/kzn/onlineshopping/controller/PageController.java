f46f72fad642a05d7d0499bfbf4d3566a79d1bda
Noman46 <nomanibrahim7@gmail.com>
2019-02-22 17:47:22 +0600

    Dummy DAO is Done

--- onelineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onelineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,16 +1,23 @@
 package net.kzn.onlineshopping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.kzn.shoppingbackend.dao.CategoryDAO;
+
 @Controller
 public class PageController {
-
+    
+	@Autowired
+	private CategoryDAO categoryDAO;
+	
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {		
 		ModelAndView mv = new ModelAndView("page");		
 		//mv.addObject("msg","Whatever you want");
+		mv.addObject("categories", categoryDAO.list());
 		mv.addObject("title","Home");
 		mv.addObject("userClickHome", true);
 		return mv;				
